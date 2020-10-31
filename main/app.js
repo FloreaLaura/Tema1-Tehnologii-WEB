3a0238469c56083693b22f182484a59b1ea3b343
FloreaLaura <florealaura18@stud.ase.ro>
2020-10-31 17:07:42 +0200

    Tema1

--- main/app.js
+++ main/app.js
@@ -1,5 +1,30 @@
 function removeOrderItem(orderInfo, position){
-    
+    //a)
+   if(Array.isArray(orderInfo.items)==false){
+    throw "Items should be an array"
+}
+
+//b)
+orderInfo.items.forEach(elem=>{
+ if(!elem.price&&!elem.quantity)
+ throw "Malformed item"
+});
+
+//c)
+if ( position<0 || position > orderInfo.items.length -1  ) {
+ throw 'Invalid position'
+}
+
+//d)
+ orderInfo.items.splice(position, 1);
+
+//e) 
+ orderInfo.total = 0
+ orderInfo.items.forEach((elem) => {
+     orderInfo.total += (elem.price * elem.quantity)
+ })
+
+ return orderInfo
 }
 
 const app = {
